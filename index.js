722212d36c82922967994ffdbf9bf00e80dc6ba1
Jamilly Anunciada <jamillymilly10@hotmail.com>
2022-11-27 22:21:43 -0300

    add icon-check and style changes
    incomplete update

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ function Exibir() {
     if (entrada.value == "") {
         overlay()
         return
-
     }
+
     var divList = document.getElementById('list');
-    let item = document.createElement('div')
-    let itemText = document.createElement('div')
-    let itemActions = document.createElement('div')
+    let item = document.createElement('div');
+    let itemText = document.createElement('div');
+    let itemActions = document.createElement('div');
+    let itemCheck = document.createElement('svg');
+    let paragrafo = document.createElement('p');
 
     divList.appendChild(item)
 
-    
     item.appendChild(itemText)
     item.appendChild(itemActions)
-    
+
     // Mostrar e sumir opções
     item.addEventListener('mouseenter', onMouseInItem)
     item.addEventListener('mouseleave', onMouseOutItem)
     // end
 
-    let paragrafo = document.createElement('p');
-    paragrafo.textContent = entrada.value;
-
     item.classList.add('item')
     itemText.classList.add('item-text')
 
-    itemActions.outerHTML += '<div class="item-actions"><svg xmlns="http://www.w3.org/2000/svg" onclick="deleteItem(this)" class="primary-button" height="32" width="32" viewbox="0 0 48 48"><path d="M13.05 42q-1.25 0-2.125-.875T10.05 39V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-16.6 24.2h3V14.75h-3Zm8.3 0h3V14.75h-3Zm-13.6-24.2V39Z"/></svg><svg xmlns="http://www.w3.org/2000/svg" class="secondary-button" onclick="editItem(this)" height="32" width="32" viewbox="-5 0 58 58"><path d="M9 47.4q-1.2 0-2.1-.9-.9-.9-.9-2.1v-30q0-1.2.9-2.1.9-.9 2.1-.9h20.25l-3 3H9v30h30V27l3-3v20.4q0 1.2-.9 2.1-.9.9-2.1.9Zm15-18Zm9.1-17.6 2.15 2.1L21 28.1v4.3h4.25l14.3-14.3 2.1 2.1L26.5 35.4H18v-8.5Zm8.55 8.4-8.55-8.4 5-5q.85-.85 2.125-.85t2.125.9l4.2 4.25q.85.9.85 2.125t-.9 2.075Z"/></svg></div>'
+    itemCheck.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M9 42q-1.2 0-2.1-.9Q6 40.2 6 39V9q0-1.2.9-2.1Q7.8 6 9 6h30q1.2 0 2.1.9.9.9.9 2.1v30q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h30V9H9v30Z" /></svg>'
+    paragrafo.textContent = entrada.value;
+
+    itemText.appendChild(itemCheck);
     itemText.appendChild(paragrafo);
+    itemActions.outerHTML += '<div class="item-actions"><svg xmlns="http://www.w3.org/2000/svg" onclick="deleteItem(this)" class="primary-button" height="32" width="32" viewbox="0 0 48 48"><path d="M13.05 42q-1.25 0-2.125-.875T10.05 39V10.5H8v-3h9.4V6h13.2v1.5H40v3h-2.05V39q0 1.2-.9 2.1-.9.9-2.1.9Zm21.9-31.5h-21.9V39h21.9Zm-16.6 24.2h3V14.75h-3Zm8.3 0h3V14.75h-3Zm-13.6-24.2V39Z"/></svg><svg xmlns="http://www.w3.org/2000/svg" class="secondary-button" onclick="editItem(this)" height="32" width="32" viewbox="-5 0 58 58"><path d="M9 47.4q-1.2 0-2.1-.9-.9-.9-.9-2.1v-30q0-1.2.9-2.1.9-.9 2.1-.9h20.25l-3 3H9v30h30V27l3-3v20.4q0 1.2-.9 2.1-.9.9-2.1.9Zm15-18Zm9.1-17.6 2.15 2.1L21 28.1v4.3h4.25l14.3-14.3 2.1 2.1L26.5 35.4H18v-8.5Zm8.55 8.4-8.55-8.4 5-5q.85-.85 2.125-.85t2.125.9l4.2 4.25q.85.9.85 2.125t-.9 2.075Z"/></svg></div>'
+
+
     document.getElementById('inpt').value = "";
 
 }
@@ -43,7 +47,7 @@ function editItem(item) {
     const divText = getItemTextDiv(item).children[0]
     const paragraph = getItemTextDiv(item).children[0].children[0]
     const actionsDiv = getItemTextDiv(item).children[2]
-    
+
     const defaultParagraphValue = getItemTextDiv(item).children[0].children[0].innerHTML
     paragraph.style.display = 'none'
     actionsDiv.style.display = 'none'
@@ -51,10 +55,10 @@ function editItem(item) {
     divText.appendChild(input)
 
     console.log(divParent)
-    
+
     input.classList.add('input-item')
     divParent.appendChild(itemActionsSaveChange)
-    
+
     divParent.innerHTML += '<div class="item-actions"><svg xmlns="http://www.w3.org/2000/svg" class="primary-button success-color" height="32" width="32" viewbox="0 0 48 48"><path d="M18.9 35.7 7.7 24.5l2.15-2.15 9.05 9.05 19.2-19.2 2.15 2.15Z"/></svg><svg xmlns="http://www.w3.org/2000/svg" class="secondary-button danger-color" onclick="editItem(this)" height="32" width="32" viewbox="0 0 48 48"><path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z"/></svg></div>'
     const actionsEditDiv = divParent.children[4]
     actionsEditDiv.style.display = 'grid'
@@ -64,7 +68,7 @@ function editItem(item) {
 /* Hover item session */
 function options(item, action) {
     // if not item in edit: do it (to do)
-    if (!inEdit){
+    if (!inEdit) {
         if (action == 'open') {
             item.target.children[2].style.display = 'grid'
         } else {
@@ -74,7 +78,7 @@ function options(item, action) {
 }
 
 function onMouseInItem(item) {
-    item.target.style.backgroundColor = '#fff'
+    item.target.style.background = 'rgba(255,255,255,.4)'
     options(item, 'open')
 }
 
@@ -114,7 +118,7 @@ function focusIn(item) {
 
 function overlay(action = 'open') {
     let overlay = document.getElementById('overlay')
-    
+
     if (action == 'open') {
         overlay.style.display = 'grid'
 
